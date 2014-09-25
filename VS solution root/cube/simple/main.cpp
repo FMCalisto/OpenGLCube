e1fb2e3b6d641e2c4031fa7509f241e3b740eade
Francisco Maria Calisto <francisco.calisto@tecnico.ulisboa.pt>
2014-09-25 23:24:26 +0100

    First Commit
    Commit the first files after being lazy for the command prompt.

---
+++ VS solution root/cube/simple/main.cpp
@@ -0,0 +1,26 @@
+#include <GL/glut.h>
+
+#include "cubeSimple.h"
+
+
+int main(int argc, char **argv)
+{
+	glutInit(&argc, argv); // Initialize GLUT
+
+	glutInitDisplayMode(GLUT_DOUBLE); // Set up a basic display buffer (now double buffered)
+	glutInitWindowSize(500, 500); // Set the width and height of the window
+	glutInitWindowPosition(100, 100); // Set the position of the window
+
+	glutCreateWindow("Your first OpenGL Window"); // Set the title for the window
+
+	glutDisplayFunc(display); // Tell GLUT to use the method "display" for rendering
+
+	glutIdleFunc(display); // Tell GLUT to use the method "display" as our idle method as well
+
+	glutReshapeFunc(reshape); // Tell GLUT to use the method "reshape" for reshaping
+
+	glutKeyboardFunc(keyPressed); // Tell GLUT to use the method "keyPressed" for key presses
+	glutKeyboardUpFunc(keyUp); // Tell GLUT to use the method "keyUp" for key up events
+
+	glutMainLoop(); // Enter GLUT's main loop
+}
