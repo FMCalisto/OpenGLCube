b56905e0f3c14753554f639c115fb21663b41836
FMCalisto <francisco_mcalisto@hotmail.com>
2014-09-26 17:37:01 +0100

    Add Xcode Files
    Adding the Xcode files.

---
+++ Xcode solution root/cubeSimple.h
@@ -0,0 +1,18 @@
+// Include the GLUT header file
+
+#if defined(__APPLE__) || defined(MACOSX)
+	#include <GLUT/glut.h>
+#else
+	#include <GL/glut.h>
+#endif
+
+
+void keyOperations(void);
+
+void display(void);
+
+void reshape(int width, int height);
+
+void keyPressed(unsigned char key, int x, int y);
+
+void keyUp(unsigned char key, int x, int y);
